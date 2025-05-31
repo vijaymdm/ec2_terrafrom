40ea08d21559307c8b364a38e5c5ccae0a5cbf00
vijay babu <vijaymdm@gmail.com>
2025-05-31 11:04:11 +0530

    update

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,10 @@
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+resource "aws_instance" "App" {
+  ami           = "ami-08a52ddb321b32a8c"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name             = "Lambda_Scheduler"
+    Lambda_Scheduler = "office_hours"
   }
+
 }
